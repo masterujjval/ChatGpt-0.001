a3b24d7ba7fddcfcdcfb1ceb595bdd817febf86e
Ujjwal gupta <guptarickey3@gmail.com>
2024-02-10 15:51:19 +0530

    Update simple-ml-convrse.js

--- simple-ml-convrse.js
+++ simple-ml-convrse.js
@@ -8,29 +8,30 @@ let d=new Date();
 
 let obj=
 {
-    "what is your name" : "My name is cosmos",
-    "whats your name" : "I'm cosmos",
-    "i love you" : "I love you too",
+    "What is your name" : "My name is cosmos",
+    "what's your name" : "I'm cosmos",
+    "I love you" : "I love you too",
     "who created you"  : "My master created me!",
-    "who lost the world cup recently" : "Australia lost the world cup",
+    "who lost the World Cup recently" : "Australia lost the World Cup",
     "today date" : `Today date and time is ${d}`,
     "date" : `Today date and time is ${d}`,
     "day" : `Today date and time is ${d}`,
     "today day" : `Today date and time is ${d}`,
     "time" : `Today date and time is ${d}`,
     "what time it is" : `Today date and time is ${d}`,
-    "who is your master" : "Overlord sir ujjwal",
-    "which is the best language to learn" : "Obviously thats Javascript",
+    "Who is your master" : "Overlord sir ujjwal",
+    "which is the best language to learn" : "Obviously that's Javascript",
     "best programming language" : "That's one and only javascript",
-    "who are you" : "I'm you",
-    "i want to add something" : "You can add questions by making chaneges in github repo",
+    "Who are you" : "I'm you",
+    "I want to add something" : "You can add questions by making changes in GitHub repo",
     "fuck you" : "Fuck you too",
-    "tell me a joke" : "Why math book is sad? because it has too many problems.",
+    "tell me a joke" : "Why math book is sad? Because it has too many problems.",
     "hy":"Hello human",
     "hi":"Hello human",
     "how are you" : "Im good as always",
-    "im also fine" :"That's so good",
-    "im also good" : "Better than being bad",
+    "I'm also fine" :"That's so good",
+    "I'm also good" : "Better than being bad",
+    
 
 }
 
